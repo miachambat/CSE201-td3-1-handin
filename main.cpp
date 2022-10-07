33b77ec7ce3c102e9431eb58271bb65026e580a1
miachambat <104329477+miacha123@users.noreply.github.com>
2022-10-07 17:45:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,4 @@
-/**
- * CSE201 - TD 3
- *
- * Work on your implementation in the td3.cpp file.
- *
- * You should not change any other file, with the exception of main.cpp
- * if you wish to debug your code.
- *
- * The main function in main.cpp runs the automatic grader by default (this is
- * what you need to run while writing your solutions).
- *
- * The value of the macro GRADING defines what code is executed in the main function.
- * If the value is 1 the program runs the automatic grading of the assignment.
- *
- * If the value is 0 (or more precisely, different from 1) the program runs the
- * code in the custom code section below (between the #if GRADING != 1 and
- * #else directives).
- *
- * You can always run the automatic grader just changing the definition of
- * GRADING to 0.
- */
-
 #define GRADING 0
-
 #include <iostream>
 #include "grading/grading.hpp"
 #include "td3.hpp"
@@ -35,7 +12,7 @@ int main(int argc, char* argv[])
   // This code will be executed only if you set GRADING to a value different from 1
 
   {
-        std::cout << "First git commit" << std::endl;
+        std::cout << "A better message" << std::endl;
 
   }
   // END OF THE CUSTOM CODE SECTION
